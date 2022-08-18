1fe911bdd9629b7472b1269b5f7cb376624d6e23
Francesca Santoriello <70381414+francescamilk@users.noreply.github.com>
2022-08-18 13:11:50 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'gardens#index'
-  resources :gardens do
+  resources :gardens, only: [ :index, :show ] do
     # GET localhost:3000/gardens/:garden_id/plants/new
     resources :plants, only: [ :new, :create ]
   end
