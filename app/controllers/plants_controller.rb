fbb08c54061319caf92768d64118a75f7a177520
Francesca Santoriello <70381414+francescamilk@users.noreply.github.com>
2022-08-18 13:17:34 +0200

    Update plants_controller.rb

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,34 @@
 class PlantsController < ApplicationController
+  before_action :set_garden, only: [:new, :create]
+  
   def new
     # params[:garden_id]
-    @garden = Garden.find(params[:garden_id])
     @plant = Plant.new
   end
 
   def create
-    @garden = Garden.find(params[:garden_id])
     @plant = Plant.new(plant_params)
     @plant.garden = @garden
 
-    redirect_to garden_path(@garden) if @plant.save
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @plant = Plant.find(params[:id])
     @plant.destroy
 
-    redirect_to garden_path(@garden)
+    redirect_to garden_path(@garden), status: :see_other
   end
 
   private
+  
+  def set_garden
+    @garden = Garden.find(params[:garden_id])
+  end
 
   def plant_params
     params.require(:plant).permit(:name, :banner_url)
