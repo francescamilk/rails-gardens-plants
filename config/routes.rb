aa9eb3a6b6aa8367ea33d39c70a2b4a5757d5135
Francesca Santoriello <70381414+francescamilk@users.noreply.github.com>
2022-08-18 13:12:41 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     # GET localhost:3000/gardens/:garden_id/plants/new
     resources :plants, only: [ :new, :create ]
   end
+  # DELETE localhost:3000/plants/:id
   resources :plants, only: :destroy
 end
